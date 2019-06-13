eb3364ba8e840f5b075fe3972ceb0e38966a273b
liucf <liucf@zjlantone.com>
2019-06-13 16:50:44 +0800

    新增接口请求(跨域处理)

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import './test.css';
 import './Test.less';
+import {post,urls} from '../config'
 
 class Test extends React.Component{
 	constructor(props){
@@ -9,12 +10,18 @@ class Test extends React.Component{
 			flag:true
 		}
 	}
+  handleClick(e){
+    post(urls.getMessage,{}).then((res)=>{
+      console.log("请求成功：",res)
+    })
+  }
   render(){
   	const {flag} = this.state;
     return <div className={'box'}>
       <p className={'text border'}>test webpackConfigaration in react project</p>
       <p className={`tip ${flag?"border":null}`}>动态添加多个类名（ES6写法）</p>
       <p className={["tip",flag?"border":null].join(' ')}>动态添加多个类名（普通写法）</p>
+      <span className={"btn"} onClick={this.handleClick}>请求数据</span>
     </div>
   }
 }
